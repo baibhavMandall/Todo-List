def3335a49f7025c3bb7462885ae32452e66f3a2
baibhavmandall <baibhavktr05@gmaail.com>
2024-08-31 14:10:07 +0530

    Added feature that helps to add now element in list

--- frontend/src/script.js
+++ frontend/src/script.js
@@ -1,6 +1,24 @@
 let add = document.querySelector('#add');
-let todoList = document.querySelector('#todo-list')
-let sider = document.querySelector('#sider');
-let listSection = document.querySelector('#list-section')
-let setting = document.querySelector('#settings');
-let feedback = document.querySelector('#feedback');
+let todoList = document.querySelector('#todo-list');
+let container = document.querySelector('#container');
+
+// let listSection = document.querySelector('#list-section');
+// let setting = document.querySelector('#settings');
+// let feedback = document.querySelector('#feedback');
+
+add.addEventListener('click', () => {
+    let userInput = prompt('Enter task name')
+    if (userInput) {
+        let newTask = document.createElement('h1');
+        newTask.id = 'list-name';
+        newTask.textContent = userInput;
+
+        let addList = document.createElement('div') 
+        addList.appendChild(newTask);
+        addList.id = 'todo-list';
+        container.appendChild(addList);
+    }
+    else {
+        alert('Task name cannot be empty!')
+    }
+})
