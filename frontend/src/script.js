3586adcb1dc48a338db465446c4d0c6ca65eeefb
baibhavmandall <baibhavktr05@gmaail.com>
2024-08-31 11:13:25 +0530

    Created side bar

--- frontend/src/script.js
+++ frontend/src/script.js
@@ -1,1 +1,6 @@
-console.log("Hello world!");
+let add = document.querySelector('#add');
+let todoList = document.querySelector('#todo-list')
+let sider = document.querySelector('#sider');
+let listSection = document.querySelector('#list-section')
+let setting = document.querySelector('#settings');
+let feedback = document.querySelector('#feedback');
